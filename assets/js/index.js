747d25a4021514d4f0628a9fdb94d3608e272c52
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-13 18:54:37 +0900

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,35 @@
 // sass
 import '../sass/app.scss';
+
+// js modules
+import 'bootstrap';
+import 'slick-carousel';
+
+$(function ($) {
+  // スムーススクロール
+  $('.smooth').on('click', function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+  // slick
+  $('.mv-slider').slick({
+    dots: false,
+    arrows: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    responsive: [{
+      breakpoint: 991.98,
+      settings: {
+        // slidesToShow: 1,
+      }
+    }]
+  });
+});
