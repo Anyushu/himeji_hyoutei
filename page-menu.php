2aea4bf131bc057343b97578577511efcd970f25
ShunyaSuzuki <anyushu2017@gmail.com>
2020-04-16 15:06:36 +0900

    about fix

--- page-menu.php
+++ page-menu.php
@@ -10,5 +10,195 @@ $wp_url = get_template_directory_uri();
 <h2><span class="text-white">お品</span>書き</h2>
 </div>
 </section>
+<section class="sec">
+<div class="container">
+<div class="main-menu">
+<div class="row">
+<div data-aos="fade-up" class="col-md-3 order-md-last">
+<h2 class="h4 text-vertical text-md-vertical">大分県安心院<br><span class="d-md-inline d-none">　　</span>すっぽん鍋コース</h2>
+</div>
+<div data-aos="fade-up" class="col-md-9 order-md-first mt-md-0 mt-4">
+<img class="w-100" src="<?php echo $wp_url; ?>/lib/images/menu_pic_1.png" alt="">
+</div>
+</div>
+<div data-aos="fade-up" class="p-md-4 mt-md-0 mt-4">
+<table class="w-100 table table-borderless">
+<tbody>
+<tr>
+<td class="text-nowrap">10,000円</td>
+<td>前菜・すっぽんのお造り・生き血・お鍋120g（野菜付き）・〆のお食事（ラーメン・雑炊）・デザート</td>
+</tr>
+<tr>
+<td class="text-nowrap">12,000円</td>
+<td>10,000円コース + すっぽんの唐揚げ付き</td>
+</tr>
+<tr>
+<td class="text-nowrap">13,000円</td>
+<td>10,000円コース + 旬のお造り付き</td>
+</tr>
+<tr>
+<td class="text-nowrap">15,000円</td>
+<td>10,000円コース + すっぽんの唐揚げ + 旬のお造り</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</section>
+<div class="container menu-main-slide">
+<div data-aos="fade-up" class="row mx-0">
+<div class="col px-md-3 px-1">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_2.png" alt="">
+</div>
+<div class="col px-md-3 px-1">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_3.png" alt="">
+</div>
+<div class="col px-md-3 px-1">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_4.png" alt="">
+</div>
+</div>
+</div>
+<section id="menu-list" class="sec">
+<div class="container">
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">ハモ鍋コース<br>ハモとアワビ鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_5.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・天麩羅・〆のお食事・デザート</p>
+<p class="mb-0">ハモ鍋 10,000円〜　ハモとアワビ鍋 12,000円〜</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">クマ鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_6.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円、12,000円、15,000円</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">キジ鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_7.png" alt="">
+<p class="mt-3 mb-1">海鮮４種（当日のおまかせ）と肉２種（すっぽん、⽜、猪、鴨、豚の中から）
+<br>前菜・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円〜</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">ウツボ鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_8.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円〜</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">⿊⽑和⽜鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_9.png" alt="">
+<p class="mt-3 mb-1">（すきやきor しゃぶしゃぶ）<br>前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円、12,000円、15,000円</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">かも鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_10.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円〜</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">いのしし鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_11.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円〜</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">河豚鍋コース<small>（冬季限定）</small></h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_12.png" alt="">
+<p class="mt-3 mb-1">前菜・てっさ・お鍋（野菜付き）・唐揚げ・〆のお食事・デザート</p>
+<p class="mb-0">10,000円、12,000円、15,000円</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">クエ鍋コース<small>（冬季限定）</small></h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_13.png" alt="">
+<p class="mt-3 mb-1">前菜・クエ刺し・お鍋（野菜付き）・唐揚げ・〆のお食事・デザート</p>
+<p class="mb-0">10,000円、12,000円、15,000円</p>
+</div>
+</div>
+</div>
+<div data-aos="fade-up" class="menu-list">
+<div class="row">
+<div class="col-md-2">
+<h3 class="h4 text-vertical text-md-vertical">瀬⼾内ワタリ蟹鍋コース</h3>
+</div>
+<div class="col-md-10 mt-md-0 mt-4">
+<img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_14.png" alt="">
+<p class="mt-3 mb-1">前菜・旬のお造り・お鍋（野菜付き）・〆のお食事・デザート</p>
+<p class="mb-0">10,000円〜</p>
+</div>
+</div>
+</div>
+</div>
+</section>
+<section class="sec">
+<div class="container">
+<h2 class="text-center h4 mb-5">おまかせ会席</h2>
+<p class="text-center mb-5">食材のリクエストがあればお申し付けください。
+<br>また、アレルギーや苦手な食材もございましたら、合わせてお申し付けください。</p>
+<p class="text-center small">コース料理一律<br>10,000円、12,000円、15,000円</p>
+<div class="mt-5">
+<img class="w-100" src="<?php echo $wp_url; ?>/lib/images/menu_pic_15.png" alt="">
+</div>
+<div class="mt-2 d-flex justify-content-between align-items-center flex-nowrap">
+<div><img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_16.png" alt=""></div>
+<div class="mx-2"><img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_17.png" alt=""></div>
+<div><img class="img-fluid" src="<?php echo $wp_url; ?>/lib/images/menu_pic_18.png" alt=""></div>
+</div>
+</div>
+</section>
 <?php
 get_footer();
